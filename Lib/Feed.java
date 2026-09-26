eaf6e9e390dbac037b787e4d4e903e8e7f159225
phisit-kongson <phisit.ko@ku.th>
2026-09-26 23:49:18 +0700

    Add files via upload

--- Lib/Feed.java
+++ Lib/Feed.java
@@ -1,5 +1,42 @@
 package Lib;
 
-public class Feed {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Feed extends JFrame implements ActionListener {
+
+    private Container cp;
+    private JButton backBtn;
+    private JFrame parentFrame; 
+
+    public Feed(JFrame parentFrame) {
+        this.parentFrame = parentFrame;
+
+        setTitle("Feed Room");
+        setSize(400, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        cp = getContentPane();
+        cp.setLayout(null);
+
+        
+        backBtn = new JButton("back");
+        backBtn.setBounds(130, 500, 140, 40);
+        backBtn.setFocusable(false);
+        backBtn.addActionListener(this);
+        cp.add(backBtn);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == backBtn) {
+            if (parentFrame != null) {
+                parentFrame.setVisible(true); // เปิดหน้า GameFrame กลับมา
+            }
+            this.dispose();
+        }
+    }
 }
